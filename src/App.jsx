f6bd3750d9434ac0249615511d51a281b5915260
Sara Maya <amii.lee.xd@gmail.com>
2023-11-01 18:01:17 -0500

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,60 @@
+import { useState } from "react";
+import Card from "./Card";
+import "./index.css";
+
 function App() {
+  const [formData, setFormData] = useState({ name: "", lastname: "" });
+  const [items, setItems] = useState([]);
+
+  console.log("items", items);
+  const fullNames = [];
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
   //Aqui deberias agregar los estados y los handlers para los inputs
+
+  const handleAddItem = (e) => {
+    e.preventDefault();
+    if (formData.name && formData.lastname) {
+      const newItem = { ...formData };
+      setItems([...items, newItem]);
+      setFormData({ name: "", lastname: "" });
+    }
+  };
 
   return (
     <div className="App">
-      <h1>Elige un color</h1>
-      <form>{/* aqui deberias escribir tu codigo */}</form>
+      <h1>Tu gato según tu nombre </h1>
+      <p>Creado por Sara Acevedo Maya {"<3"}</p>
+      <form onSubmit={handleAddItem}>
+        <div className="formClass">
+          <input
+            type="text"
+            name="name"
+            placeholder="Nombre"
+            value={formData.name}
+            onChange={handleInputChange}
+          />
+          <input
+            type="text"
+            name="lastname"
+            placeholder="Apellido"
+            value={formData.lastname}
+            onChange={handleInputChange}
+          />
+          <button type="submit">Agregar</button>
+        </div>
+      </form>
+      <div className="container">
+        {items.map((item, index) => (
+          <Card key={index} item={item} />
+        ))}
+      </div>
     </div>
   );
 }
--- src/Card.jsx
+++ src/Card.jsx
@@ -1,10 +1,23 @@
 //Este componente deberia recibir por props y mostrar en pantalla la informacion
 //que envia el usuario
+import "./index.css";
 
-function Card() {
+function Card(props) {
+  const { item } = props;
+  console.log("item", item);
   return (
-    <div>
-      <h2>Esto es un componente</h2>
+    <div className="card">
+      <p>
+        Este es tu gato según tu nombre:{" "}
+        <b>
+          {item?.name} {item?.lastname}
+        </b>{" "}
+      </p>
+      <img
+        src={`https://cataas.com/cat/says/${item?.name}?fontSize=50&fontColor=yellow`}
+        alt={item?.name}
+        className="image"
+      />
     </div>
   );
 }
